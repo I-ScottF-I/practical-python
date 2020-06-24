59731352051fcf0897c0abdb9361b7ab68ef81c7
S <binbag160@gmail.com>
2020-06-24 10:17:36 +0100

    bounce done

---
+++ Work/NonExerciseCode/testing.py
@@ -0,0 +1,10 @@
+a = 1
+b = 3
+
+#testing various loops n things
+
+while b > a:
+    a = a+1
+    if b > a:
+        print ('b',b, 'is greater than a',a)
+    print (a)
--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,18 @@
 # bounce.py
 #
 # Exercise 1.5
+
+#ball hits the ground from height of 100m and every time 
+#it bounces its apex is 3/5ths of its original bounce
+
+#show height of first 10 bounces
+
+height = 100
+bounces = 0
+
+while bounces < 10:
+    height = height * 0.6
+    bounces = bounces + 1
+    print (bounces, height)
+    
+    
