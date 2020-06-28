9a0ef37ca89115c45733a17b73dc0c0b83bebba0
S <binbag160@gmail.com>
2020-06-28 01:03:35 +0100

    Finished task 1.7

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,27 @@
 # pcost.py
 #
 # Exercise 1.27
+#
+#Now that you know how to read a file, let’s write a program to perform a simple calculation.
+#
+#The columns in `portfolio.csv` correspond to the stock name, number of
+#shares, and purchase price of a single stock holding.  Write a program called
+#`pcost.py` that opens this file, reads all lines, and calculates how
+#much it cost to purchase all of the shares in the portfolio.
+#
+#*Hint: to convert a string to an integer, use `int(s)`. To convert a string to a floating point, use `float(s)`.*
+#
+#Your program should print output such as the following:
+#
+#```bash
+#Total cost 44671.15
+#```
+
+purchaseCost = 0
+import gzip
+with gzip.open('Data/portfolio.csv.gz', 'rt') as dataSheet:
+    headers = next(dataSheet)
+    for line in dataSheet:
+        row = line.split(',')
+        purchaseCost = purchaseCost + int(row[1]) * float(row[2])
+print(purchaseCost)
